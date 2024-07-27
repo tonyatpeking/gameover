81ddef7f7f2c07878268b86962d1b721778a8788
tonyatpeking <tonyatpeking@gmail.com>
2024-07-26 20:24:18 -0500

    testing stuff out

--- src/extension.ts
+++ src/extension.ts
@@ -9,18 +9,47 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "game-over" is now active!');
+	vscode.commands.executeCommand('setContext', 'game-over.moveMode', true);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('game-over.helloWorld', () => {
+	//#region commands
+	let disposable = vscode.commands.registerCommand('game-over.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		let message = 'Hello World from game-over! Modified';
 		vscode.window.showInformationMessage(message);
 	});
-
 	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('game-over.i', () => {
+		vscode.commands.executeCommand('vscode.executeWorkspaceSymbolProvider', 'a').then(
+			function (symbols) {
+				console.log(symbols);
+			});
+
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('game-over.j', () => {
+		vscode.commands.executeCommand('cursorWordEndLeftSelect');
+		console.log('j');
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('game-over.k', () => {
+		vscode.commands.executeCommand('cursorWordStartLeftSelect');
+		console.log('k');
+	});
+	context.subscriptions.push(disposable);
+
+	disposable = vscode.commands.registerCommand('game-over.l', () => {
+		vscode.commands.executeCommand('cursorWordLeftSelect');
+		console.log('l');
+	});
+	context.subscriptions.push(disposable);
+	//#endregion
 }
 
 // This method is called when your extension is deactivated
---
+++ src/test/test-dir/scratchpad.py
@@ -0,0 +1,25 @@
+def my_function():
+    # Do some work here
+    my_function.custom_attribute = 'Hello, World!'
+
+
+# Call the function to set the attribute
+my_function()
+
+# Access the function attribute
+print(my_function.custom_attribute)  # Output: Hello, World!
+
+
+def aa():
+    aa.abc = 'ab'
+    print('aa')
+    asdfa = '''
+sdf, asdf sd = [sdfs] asdf
+sdf
+sdfasdf
+'''
+
+
+aa()
+
+print(aa.abc)
